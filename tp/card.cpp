bb77f97d03130335c2d58e43280d1bb0fa4aae4d
beyxyy <antho.kalbe@gmail.com>
2025-12-12 21:11:07 +0100

    mise au propre

--- tp/card.cpp
+++
@@ -1,7 +0,0 @@
-#include "header/card.h"
-#include <string>
-
-Card::Card(const std::string name) : cardName(name){}
-std::string Card::getCardName() const{
-    return this->cardName;
-}
--- tp/trainerCard.cpp
+++
@@ -1,9 +0,0 @@
-#include "header/trainerCard.h"
-
-TrainerCard::TrainerCard() : 
-trainerEffect("heal all your action pokemon"), Card("trainer")
-{}
-
-std::string TrainerCard::getTrainerEffect() const {
-    return this->trainerEffect;
-}
